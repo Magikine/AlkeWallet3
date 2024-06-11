25a5b815a193eabb521f869369665733feb3603e
Magikine <marjorie.moya@gmail.com>
2024-06-11 17:41:16 -0400

    este si x 2

--- app/src/main/java/marjoriemoya/miniguamvvm/view/adapter/HomePageEmptyCaseActivity.kt
+++ app/src/main/java/marjoriemoya/miniguamvvm/view/adapter/HomePageEmptyCaseActivity.kt
@@ -1,5 +1,6 @@
 package marjoriemoya.miniguamvvm.view.adapter
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import marjoriemoya.miniguamvvm.databinding.ActivityHomePageEmptyCaseBinding
@@ -11,19 +12,34 @@ class HomePageEmptyCaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Inflar el diseño de la actividad y establecerlo como contenido de la vista
         binding = ActivityHomePageEmptyCaseBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//
 
 
-        val registerResponse = UserData.registerResponse
+        // Configurar el OnClickListener para el botón de ingresar dinero
+        binding.button.setOnClickListener {
 
-        if (registerResponse != null) {
-
-
-
+            // Abrir la pantalla de ingresar dinero (Ocho_SendMoneyActivity)
+            val abrirPantallaOcho = Intent(this, SendMoneyActivity::class.java)
+            startActivity(abrirPantallaOcho)
         }
+        // Configurar el OnClickListener para el botón de enviar de dinero
+        binding.button2.setOnClickListener {
+
+            // Abrir la pantalla de enviar de  dinero (Siete_ResquestMoneyActivity)
+            val abrirPantallaSiete = Intent(this, ResquestMoneyActivity::class.java)
+            startActivity(abrirPantallaSiete)
 
 
+            finish() // Finalizar esta actividad después de abrir la pantalla de inicio de sesión
+
+
+            val registerResponse = UserData.registerResponse
+
+            if (registerResponse != null) {
+            }
+        }
     }
 }
+
--- app/src/main/java/marjoriemoya/miniguamvvm/view/adapter/LoginSignupPageActivity.kt
+++ app/src/main/java/marjoriemoya/miniguamvvm/view/adapter/LoginSignupPageActivity.kt
@@ -3,6 +3,7 @@ package marjoriemoya.miniguamvvm.view.adapter
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import marjoriemoya.miniguamvvm.databinding.ActivityLoginSignupPageBinding
 
@@ -33,15 +34,15 @@ class LoginSignupPageActivity : AppCompatActivity() {
             finish() // Finalizar esta actividad después de abrir la pantalla de inicio de sesión
         }
 
-        //Rescatando los valores de la pantalla anterior
-//        val nombre = intent.getStringExtra("nombre")
-//        val apellido = intent.getStringExtra("apellido")
-//        val tyc = intent.getBooleanExtra("acepto_tyC", false)
+      //  Rescatando los valores de la pantalla anterior
+  val nombre = intent.getStringExtra("nombre")
+        val apellido = intent.getStringExtra("apellido")
+        val tyc = intent.getBooleanExtra("acepto_tyC", false)
         //Vamos a mostrar el dato en toast
-//        Toast.makeText(this,
-//            "Hola $nombre $apellido -- Acepto los tyc $tyc",
-//            Toast.LENGTH_SHORT)
-//            .show()
+        Toast.makeText(this,
+            "Hola $nombre $apellido -- Acepto los tyc $tyc",
+            Toast.LENGTH_SHORT)
+            .show()
 
 
     }
